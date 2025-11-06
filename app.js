d3a9943b230886cbf414e2e6deab1b23532ae2be
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-06 13:35:35 -1000

    excersise 2 issue

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(kuhljkhcv)
+/*
+caluclating price and responses
+*/
+let cash = 30
+let price = 40
+
+function autoCalculate (cash , price){
+
+    // Automatically compute and show result
+}
+      
+    if (cash > price)  {
+        const change 
+        = cash - price; 
+        console.log ("you paid extra! Here is $${change} change.");
+    }
+    
+    else if (cash === price)
+        {
+            console.log("You paid the excat amount. Have a nice day!");
+        }
+
+    else {
+            const owed = price - cash;
+            console.log("Sorry, not enough money. You still owe "$${owed.toFixed(2)"}
+            .);}  
