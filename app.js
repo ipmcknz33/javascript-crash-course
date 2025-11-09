3cd71ccc246409a439f5f6b987742dd437a249bb
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-09 10:21:38 -1000

    my console isnt showing resuts blank screen

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-/*
-caluclating price and responses
-*/
-let cash = 30
-let price = 40
+// DRY = DO NOT REPEAT YOURSELF
+let number = 1;
 
-function autoCalculate (cash , price){
-
-    // Automatically compute and show result
+while (count <= 100) {
+    console.log(`this ran`);
+    count = count + 1;
 }
-      
-    if (cash > price)  {
-        const change 
-        = cash - price; 
-        console.log ("you paid extra! Here is $${change} change.");
-    }
-    
-    else if (cash === price)
-        {
-            console.log("You paid the excat amount. Have a nice day!");
-        }
 
-    else {
-            const owed = price - cash;
-            console.log("Sorry, not enough money. You still owe "$${owed.toFixed(2)"}
-            .);}  
