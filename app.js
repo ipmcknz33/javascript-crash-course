efc8cdd6aee4e64728d8a9275a71dc7e81acd67a
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-18 12:53:05 -1000

    title to red

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ document.querySelector(".title").innerHTML +=  "Frontend Simplified"
 // change css
 document.querySelector(".title").style.fontsize = '10px'
 
-function toggleButtonColor() {
-    console.log("clicked");
-   const button = document.querySelector(".btn");
-
-    button.classList.toggle("is-red"); 
+function changeTitleToRed() {
+     document.querySelector(".title").style.color = 'red'
 }
 
 
