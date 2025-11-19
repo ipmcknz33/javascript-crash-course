567df845ca6fdb67f2984a964ed1168ca04a663d
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-19 12:16:37 -1000

    promise challenge issue

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-// change html
-document.querySelector(".title").innerHTML +=  "Frontend Simplified"
+const status = document.querySelector("#status");
 
-// change css
-document.querySelector(".title").style.fontsize = '10px'
-
-function changeTitleToRed() {
-     document.querySelector(".title").style.color = 'red'
+function getSubscriptionStatus() {
+    return new Promise ((resolve, reject) => {
+        setimeout (() = {
+            resolve ("VIP");
+        }, 2000);
+    });
+}
+function getVideo(getSubscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve ("show video")
+        } 
+        else if (subscription === "FREE")
+            resolve ("show trailer")
+        {
+        else {
+          reject ("no video");
+        }
+    })
+}
+async function main() {
+    const status = await getSubscriptionStatus();
+    statusRef .innerHTML = status;
+    console.log(await getvideo(status))
 }
 
-
+main();
 
 
