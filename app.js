4aa55287f32406c83d2625eaeb61ad2a5a1803dd
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-22 13:26:54 -1000

    hoursIntoSeconds

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-const status = document.querySelector(".status");
-
-function getSubscriptionStatus() {
-    return new Promise ((resolve, reject) => {
-        setTimeout (() => {
-            resolve ("VIP");
-        }, 2000);
-    });
+function hoursIntoSeconds(hours) {
+    return hours * 3600;
 }
-function getVideo(subscriptionStatus) {
-    return new Promise((resolve, reject) => {
-        if (subscriptionStatus === "VIP") {
-            resolve ("show video");
-         } else if (subscriptionStatus === "FREE") {
-            resolve ("show trailer"); 
-          } else {
-          reject ("no video");
-        }
-    });
-}
-async function main() {
-    const status = await getSubscriptionStatus();
-    status.innerHTML = status;
-    console.log(await getVideo(status))
-}
-
-main();
-
-
+    console.log(hoursIntoSeconds(2));
+    console.log(hoursIntoSeconds(10));
+    console.log(hoursIntoSeconds(24));
