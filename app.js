9620ac29864e258f53d78dea853e34ea304ada00
ipmcknz33 <i.p.mcknz33@gmail.com>
2025-11-23 07:44:18 -1000

    sumGreaterThan100

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-function appendFrontend (str) {
-    return str + "Frontend";
-}
-
-    console.log(appendFrontend("Apple"));
-    console.log(appendFrontend("Bannana"));
-    console.log(appendFrontend("Orange"));
+function sumGreaterThan100(num1, num2) {
+    return num1 + num2 > 100; 
+    }
+    
+    console.log(sumGreaterThan100("20,10")); 
+    console.log(sumGreaterThan100("50,60")); 
+    console.log(sumGreaterThan100("100,-50")); 
 
 
 
